249f078f03395db7b0475e8d6cbe2ca88a21f924
jumper9222 <chindidken@gmail.com>
2024-12-09 21:37:57 +0800

    Initial commit

---
+++ src/App.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App"
+import { expect, test } from "vitest";
+import userEvent from "@testing-library/user-event";
+
+test("App form submission", async () => {
+    render(<App />);
+
+    const emailInput = screen.getByTestId("emailInput");
+    const submitButton = screen.getByTestId("submitButton");
+
+    await userEvent.type(emailInput, 'sigmaschool@gmail.com');
+
+    expect(submitButton.disabled).toBe(false);
+})
