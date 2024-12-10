a8a133d602aef40d8e4c58ff980bc574ef20739f
jumper9222 <chindidken@gmail.com>
2024-12-10 22:03:20 +0800

    Added password field and test

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,27 @@ import { Button, Container, Form } from "react-bootstrap";
 
 export default function App() {
     const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const [isFormValid, setIsFormValid] = useState(false)
+    const [isPasswordValid, setIsPasswordValid] = useState(false)
 
     const checkFormValidity = () => {
         const form = document.querySelector('form');
         setIsFormValid(form.checkValidity());
     };
 
+    const checkPasswordValidity = () => {
+        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/
+        setIsPasswordValid(regex.test(password))
+    };
+
     useEffect(() => {
         checkFormValidity();
     }, [])
+
+    useEffect(() => {
+        checkPasswordValidity();
+    }, [password])
 
     return (
         <>
@@ -30,10 +41,21 @@ export default function App() {
                             data-testid="emailInput"
                         />
                     </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeholder="Enter password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                            data-testid="passwordInput"
+                        />
+                    </Form.Group>
                     <Button
                         type="submit"
                         className="btn btn-primary"
-                        disabled={!isFormValid}
+                        disabled={!isFormValid || !isPasswordValid}
                         data-testid="submitButton"
                     >
                         Submit
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -7,9 +7,24 @@ test("App form submission", async () => {
     render(<App />);
 
     const emailInput = screen.getByTestId("emailInput");
+    const passwordInput = screen.getByTestId("passwordInput");
     const submitButton = screen.getByTestId("submitButton");
 
     await userEvent.type(emailInput, 'sigmaschool@gmail.com');
+    await userEvent.type(passwordInput, 'Abcd1234')
 
     expect(submitButton.disabled).toBe(false);
+})
+
+test("Invalid password", async () => {
+    render(<App />);
+
+    const emailInput = screen.getByTestId("emailInput");
+    const passwordInput = screen.getByTestId("passwordInput");
+    const submitButton = screen.getByTestId("submitButton");
+
+    await userEvent.type(emailInput, 'sigmaschool@gmail.com');
+    await userEvent.type(passwordInput, 'abcd1234')
+
+    expect(submitButton.disabled).toBe(true);
 })
